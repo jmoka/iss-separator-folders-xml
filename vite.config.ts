ed11ebef235aa8cdd95e04cbcee97e71565a875a
teusiaqe <jmokatavares@gmail.com>
2025-07-24 21:07:39 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './', // Adiciona base relativa para funcionar com file://
   server: {
     host: "::",
     port: 8080,
